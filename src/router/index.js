bfd57a641776662b7b60cb6fd90974c22e428f01
大海 <1907248400@qq.com>
2019-11-19 08:49:33 +0800

    lala

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import Home from '@/views/system/Home'
 /* import Home1 from wertyuixcvghj'@/views/system/Home1'
 import Home2 from '@/views/system/Home2'
 import Home3 from '@/views/system/Home3'
- ；啦啦啦啦啦啦*/
+ ；啦啦啦啦啦啦 akjfgajk*/
 Vue.use(Router)
 
 export default new Router({
